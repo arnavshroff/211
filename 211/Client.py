60842f31d89bf90d5c68e54be1de51260399ca6b
arnavshroff <74356348+arnavshroff@users.noreply.github.com>
2023-04-04 20:36:46 +0530

    Update Client.py

--- 211/Client.py
+++ 211/Client.py
@@ -75,7 +75,7 @@ def stop():
     mixer.music.load('shared_files/'+song_selected)
     mixer.music.pause()
     infoLabel.configure(text="")
-  
+    
 def pause():
     global song_selected
     pygame
@@ -89,11 +89,30 @@ def resume():
     mixer.music.load('shared_files/'+song_selected)
     mixer.music.play() 
    
-    
-
-    
-        
-
+  
+def download():    
+    song_to_download=listbox.get(ANCHOR)
+    infoLabel.configure(text="Downloading "+ song_to_download)
+    HOSTNAME = "127.0.0.1"
+    USERNAME = "lftpd"
+    PASSWORD = "lftpd"
+    home = str(Path.home())
+    download_path=home+"/Downloads"
+    ftp_server = ftplib.FTP(HOSTNAME, USERNAME, PASSWORD)
+    ftp_server.encoding = "utf-8"
+    ftp_server.cwd('shared_files')
+    local_filename = os.path.join(download_path, song_to_download)
+    file = open(local_filename, 'wb')
+    ftp_server.retrbinary('RETR '+ song_to_download, file.write)
+    ftp_server.dir()
+    file.close()
+    ftp_server.quit()
+    infoLabel.configure(text="Download Complete")
+    time.sleep(1)
+    if(song_selected != ""):
+        infoLabel.configure(text="Now Playing: " +song_selected)
+    else:
+       infoLabel.configure(text="") 
 def musicWindow(): 
     global song_counter
     global filePathLabel
@@ -125,16 +144,17 @@ def musicWindow():
     Stop=Button(window,text="Stop",bd=1,width=10,bg='SkyBlue', font = ("Calibri",10), command = stop)
     Stop.place(x=200,y=200)
     
+     ResumeButton=Button(window,text="Resume", width=10,bd=1,bg='SkyBlue',font = ("Calibri",10), command = resume)
+    ResumeButton.place(x=30,y=250)
+
     PauseButton=Button(window,text="Pause", width=10,bd=1,bg='SkyBlue',font = ("Calibri",10), command = pause)
     PauseButton.place(x=200,y=250)
-    
-    Stop=Button(window,text="Stop",bd=1,width=10,bg='SkyBlue', font = ("Calibri",10), command = stop)
-    Stop.place(x=200,y=200)
 
+    
     Upload=Button(window,text="Upload",width=10,bd=1,bg='SkyBlue', font = ("Calibri",10), command = browseFiles)
     Upload.place(x=30,y=250)
     
-    Download =Button(window,text="Download",width=10,bd=1,bg='SkyBlue', font = ("Calibri",10))
+    Download =Button(window,text="Download",width=10,bd=1,bg='SkyBlue', font = ("Calibri",10), command = download)
     Download.place(x=200,y=250)
     
     infoLabel = Label(window, text= "",fg= "blue",bg='SkyBlue', font = ("Calibri",8))
@@ -153,12 +173,4 @@ def setup():
 
     musicWindow()
 
-
 setup()
-
-
-   
-
-
-
-
